7c87b256daea1a9fffc1c68b62331fbc54c3e739
anthonyPassler <passleranthony@yahoo.com>
2021-01-12 18:29:50 +0100

    Add new gif component

---
+++ src/components/Gif.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Gif = () => {
+  return (
+    <img className="gif" src="https://media1.giphy.com/media/13VJu6tRPDBF72/giphy.gif?cid=ecf05e472b36b1f5dcad9f2d804d6cceb1fd6cea122137ad&rid=giphy.gif" alt=""/>
+  );
+}
+
+export default Gif;
