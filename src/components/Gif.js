1c6c9b922cf8641e961afff062713b79e009912f
anthonyPassler <passleranthony@yahoo.com>
2021-01-13 18:27:28 +0100

    Add GifList component

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Gif = () => {
+const Gif = (props) => {
+  const url = `https://media1.giphy.com/media/${props.id}/giphy.gif`
   return (
-    <img className="gif" src="https://media1.giphy.com/media/13VJu6tRPDBF72/giphy.gif?cid=ecf05e472b36b1f5dcad9f2d804d6cceb1fd6cea122137ad&rid=giphy.gif" alt=""/>
+    <img className="gif" src={url} alt=""/>
   );
 }
 
